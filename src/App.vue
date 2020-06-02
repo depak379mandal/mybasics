7922e7c4b87e6ae9ddf4eeded77f4eaf24795e0d
depak379mandal <44017289+depak379mandal@users.noreply.github.com>
2020-06-02 15:36:13 +0530

    first change

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Message/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Message from './components/Message';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Message,
+  },
+  data(){
+    
+  },
+  methods:{
+    fetchData(){
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div class="text-center">
+            First Name
+            <input type="text" class="input-group" v-model="firstName">
+        </div>
+        <div class="text-center">
+            Last Name
+            <input type="text" class="input-group" v-model="lastName">
+        </div>
+        <div>
+            Full Name <span style="color:red;">{{fullName}}</span>
+        </div>
+        <button @click="count++">Add 1</button>
+        <div>
+            Value of Count : {{count}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Form',
+    data(){
+        return {
+            firstName : '',
+            lastName : '',
+            count : 0,
+            }
+    },
+    computed:{
+        fullName(){
+            return this.firstName + ' '+ this.lastName;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .form-group{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 1em 0;
+        flex-direction: column;
+    }
+    .text-center{
+        text-align: center;
+        display: block;
+    }
+    .input-group{
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 2em;
+        padding: 0;
+        margin: 1em auto;
+    }
+</style>
